3a7019f90348d5b095551547ba3a6c05b9175d53
ldg1989 <641489189@qq.com>
2021-03-23 15:35:45 +0800

    add gitignore

---
+++ RabbitMQ/RabbitMQ-Routing/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ_Routing
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // 使用命令启动 cmd 到当前目录  dotnet run 【参数】
+
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.ExchangeDeclare(exchange: "direct_logs",
+                                type: "direct");
+
+        var severity = (args.Length > 0) ? args[0] : "info";
+        var message = (args.Length > 1)
+                      ? string.Join(" ", args.Skip(1).ToArray())
+                      : "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        for (int i = 0; i < 100; i++)
+        {
+          channel.BasicPublish(exchange: "direct_logs",
+                            routingKey: severity,
+                            basicProperties: null,
+                            body: body);
+          Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+          Thread.Sleep(2000);
+        }        
+      }
+
+      Console.WriteLine(" Press [enter] to exit.");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ RabbitMQ/Simplest/RabbitMQ-Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ_Producer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "hello",// 通道
+                             durable: false, //是否 持久化
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        string message = "Hello World!";
+
+        for (int i = 0; i < 50; i++)
+        {
+          Thread.Sleep(2000);
+          message = message + DateTime.Now;
+          var body = Encoding.UTF8.GetBytes(message);
+
+          channel.BasicPublish(exchange: "",
+                               routingKey: "hello",
+                               basicProperties: null,
+                               body: body);
+          Console.WriteLine(" [x] Sent {0}", message);
+
+        }
+      }
+
+      Console.WriteLine(" Press [enter] to exit.");
+      Console.ReadLine();
+
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
