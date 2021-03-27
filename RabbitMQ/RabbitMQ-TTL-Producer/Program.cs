8b60a56b0b0d5c728be8c7be8d8e5c06215f602c
641489189@qq.com <641489189@qq.com>
2021-03-27 20:08:55 +0800

    消费者  消息确认机制

--- RabbitMQ/RabbitMQ-TTL-Producer/Program.cs
+++ RabbitMQ/RabbitMQ-TTL-Producer/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
-
+using System.ServiceModel.Channels;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQ_Routing
 {
@@ -15,6 +16,7 @@ namespace RabbitMQ_Routing
       // 使用命令启动 cmd 到当前目录  dotnet run 【参数】
 
       var factory = new ConnectionFactory() { HostName = "localhost" };
+
       using (var connection = factory.CreateConnection())
       using (var channel = connection.CreateModel())
       {
@@ -44,15 +46,62 @@ namespace RabbitMQ_Routing
                       ? string.Join(" ", args.Skip(1).ToArray())
                       : "Hello World!";
         var body = Encoding.UTF8.GetBytes(message);
+        ////未开启下次确认 
+        //for (int i = 0; i < 10; i++)
+        //{
+        //  channel.BasicPublish(exchange: exchangeName, routingKey: routingKey,
+        //                    basicProperties:null,
+        //                    body: body); 
 
+
+        //  Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+        //  Thread.Sleep(2000);
+        //}
+
+        ////事务模式 
+        //channel.TxSelect();//开启事务
+        //try
+        //{
+        //  for (int i = 0; i < 10; i++)
+        //  {
+        //    channel.BasicPublish(exchange: exchangeName, routingKey: routingKey,
+        //                      basicProperties: null,
+        //                      body: body);
+
+        //    Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+        //    Thread.Sleep(2000);
+        //  }
+        //  channel.TxCommit();//提交事务
+        //}
+        //catch (Exception e)
+        //{
+        //  var erryMsg = e.StackTrace;
+        //  channel.TxRollback();//事务回滚
+        //}
+
+        //开启消息确认 Confirm模式
+        channel.ConfirmSelect();
         for (int i = 0; i < 10; i++)
         {
           channel.BasicPublish(exchange: exchangeName, routingKey: routingKey,
                             basicProperties: null,
                             body: body);
+          if (!channel.WaitForConfirms())// 逐条 确认
+          {
+            Console.WriteLine(" 消息投递失败'{0}':'{1}'", severity, message);
+          };
           Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
           Thread.Sleep(2000);
         }
+        //if (!channel.WaitForConfirms())// 多条一起 确认
+        //{
+        //  Console.WriteLine(" 消息投递失败'{0}':'{1}'", severity, message);
+        //};
+
+
+
+
+
       }
 
       Console.WriteLine(" Press [enter] to exit.");
@@ -77,9 +126,57 @@ namespace RabbitMQ_Routing
       channel.QueueBind("dead-exchange-queue", "dead-exchange", routingKey: "dead");
 
       return channel;
-
     }
+  }
 
 
+  /// <summary>
+  /// 消费端
+  /// </summary>
+  class Consumer
+  {
+
+    static void Main(string[] args)
+    {
+      ulong tag = 1;
+
+      // 使用命令启动 cmd 到当前目录  dotnet run 【参数】
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.ExchangeDeclare(exchange: "driect_message_exchange", type: "ttl");
+        var queueName = channel.QueueDeclare().QueueName;
+
+        string routingKey = "ttl01";
+        channel.QueueBind(queue: queueName, exchange: "driect_message_exchange", routingKey: routingKey);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+        try
+        {
+          consumer.Received += (model, ea) =>
+          {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var routingKey = ea.RoutingKey;
+            // 应答机制
+            channel.BasicAck(deliveryTag: tag, multiple: false);
+
+            Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+          };
+          channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+        }
+        catch (Exception ex)
+        {
+          // 参数        消息标识  单挑  不重发 
+          channel.BasicNack(deliveryTag: tag, multiple: false, requeue: false);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+      }
+    }
   }
 }
